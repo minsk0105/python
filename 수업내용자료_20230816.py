a42b661f419a0f07745474dffeb34ca8fa801fda
minsk0105 <128563090+minsk0105@users.noreply.github.com>
2023-08-16 23:20:52 +0900

    Add files via upload

---
+++ 수업내용자료_20230816.py
@@ -0,0 +1,89 @@
+# 팩토리얼 함수로 구현하기
+def factorial(n):
+    output = 1
+    for i in range(1, n + 1):
+        output *= i
+    return output
+
+print("1!:", factorial(1))
+print("2!:", factorial(2))
+print("3!:", factorial(3))
+print("4!:", factorial(4))
+print("5!:", factorial(5))
+print("25!:", factorial(25))
+print("1300!:", factorial(1300))
+
+# 팩토리얼 재귀함수로 구현하기
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+    
+print("1!:", factorial(1))
+print("2!:", factorial(2))
+print("3!:", factorial(3))
+print("4!:", factorial(4))
+print("5!:", factorial(5))
+
+def fibonacci(n):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+    
+print("fibonacci(1):", fibonacci(1))
+print("fibonacci(2):", fibonacci(2))
+print("fibonacci(3):", fibonacci(3))
+print("fibonacci(4):", fibonacci(4))
+print("fibonacci(5):", fibonacci(5))
+
+counter = 0
+
+def fibonacci(n):
+    counter += 1
+
+    if n == 1:
+        return 1
+    if n == 2:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+    
+
+fibonacci(10)
+
+dictionary = {
+    1: 1,
+    2: 2
+}
+
+def fibonacci(n):
+    if n in dictionary:
+        return dictionary[n]
+    else:
+        output = fibonacci(n - 1) + fibonacci(n - 2)
+        dictionary[n] = output
+        return output
+
+print("fibonacci(10):", fibonacci(10))
+print("fibonacci(20):", fibonacci(20))
+print("fibonacci(30):", fibonacci(30))
+print("fibonacci(40):", fibonacci(40))
+print("fibonacci(50):", fibonacci(50))
+
+def fibonacci(n):
+    if n in dictionary:
+        return dictionary[n]
+    else:
+        output = fibonacci(n - 1) + fibonacci(n - 2)
+        dictionary[n] = output
+        return output
+    
+def fibonacci(n):
+    if n in dictionary:
+        return dictionary[n]
+    output = fibonacci[n] = output
+    return output
