71451aaf0bd0c8d52d0276f6245fbcedbfd1276e
minsk0105 <128563090+minsk0105@users.noreply.github.com>
2023-08-16 10:35:37 +0900

    Add files via upload

---
+++ 230816.py
@@ -0,0 +1,27 @@
+# 팩토리얼 함수로 구현하기
+# def factorial(n):
+#     output = 1
+#     for i in range(1, n + 1):
+#         output *= i
+#     return output
+
+# print("1!:", factorial(1))
+# print("2!:", factorial(2))
+# print("3!:", factorial(3))
+# print("4!:", factorial(4))
+# print("5!:", factorial(5))
+# print("25!:", factorial(25))
+# print("1300!:", factorial(1300))
+
+# 팩토리얼 재귀함수로 구현하기
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+    
+# print("1!:", factorial(1))
+# print("2!:", factorial(2))
+# print("3!:", factorial(3))
+# print("4!:", factorial(4))
+print("5!:", factorial(5))
